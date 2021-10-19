58c9dd66c998023aab2e6d3b81224acf9711d876
Kenneth Tang <kenneth@althea.systems>
2021-10-19 15:32:35 -0700

    Modified querir_test to include most functions from querier thus reducing querier size
    This should reduce some redundancy from querier and make things more convenient.

--- module/x/gravity/keeper/querier_test.go
+++ module/x/gravity/keeper/querier_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	gethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -75,6 +76,31 @@ func TestQueryValsetConfirm(t *testing.T) {
 	}
 }
 
+// queryValsetConfirm returns the confirm msg for single orchestrator address and nonce
+// When nothing found a nil value is returned
+func queryValsetConfirm(ctx sdk.Context, path []string, keeper Keeper) ([]byte, error) {
+	nonce, err := types.UInt64FromString(path[0])
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, err.Error())
+	}
+
+	accAddress, err := sdk.AccAddressFromBech32(path[1])
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, err.Error())
+	}
+
+	valset := keeper.GetValsetConfirm(ctx, nonce, accAddress)
+	if valset == nil {
+		return nil, nil
+	}
+	res, err := codec.MarshalJSONIndent(types.ModuleCdc, *valset)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+
+	return res, nil
+}
+
 //nolint: exhaustivestruct
 func TestAllValsetConfirmsBynonce(t *testing.T) {
 	input := CreateTestEnv(t)
@@ -133,6 +159,47 @@ func TestAllValsetConfirmsBynonce(t *testing.T) {
 			assert.JSONEq(t, string(spec.expResp), string(got))
 		})
 	}
+}
+
+// allValsetConfirmsByNonce returns all the confirm messages for a given nonce
+// When nothing found an empty json array is returned. No pagination.
+func queryAllValsetConfirms(ctx sdk.Context, nonceStr string, keeper Keeper) ([]byte, error) {
+	nonce, err := types.UInt64FromString(nonceStr)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, err.Error())
+	}
+
+	var confirms []*types.MsgValsetConfirm
+	keeper.IterateValsetConfirmByNonce(ctx, nonce, func(_ []byte, c types.MsgValsetConfirm) bool {
+		confirms = append(confirms, &c)
+		return false
+	})
+	if len(confirms) == 0 {
+		return nil, nil
+	}
+	res, err := codec.MarshalJSONIndent(types.ModuleCdc, confirms)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+
+	return res, nil
+}
+
+// Gets MaxResults batches from store. Does not select by token type or anything
+func lastBatchesRequest(ctx sdk.Context, keeper Keeper) ([]byte, error) {
+	var batches []*types.OutgoingTxBatch
+	keeper.IterateOutgoingTXBatches(ctx, func(_ []byte, batch *types.InternalOutgoingTxBatch) bool {
+		batches = append(batches, batch.ToExternal())
+		return len(batches) == MaxResults
+	})
+	if len(batches) == 0 {
+		return nil, nil
+	}
+	res, err := codec.MarshalJSONIndent(types.ModuleCdc, batches)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+	return res, nil
 }
 
 // TODO: Check failure modes
@@ -291,6 +358,25 @@ func TestLastValsetRequests(t *testing.T) {
 			assert.JSONEq(t, string(spec.expResp), string(got), string(got))
 		})
 	}
+}
+
+// lastValsetRequests returns up to maxValsetRequestsReturned valsets from the store
+func lastValsetRequests(ctx sdk.Context, keeper Keeper) ([]byte, error) {
+	var counter int
+	var valReq []*types.Valset
+	keeper.IterateValsets(ctx, func(_ []byte, val *types.Valset) bool {
+		valReq = append(valReq, val)
+		counter++
+		return counter >= maxValsetRequestsReturned
+	})
+	if len(valReq) == 0 {
+		return nil, nil
+	}
+	res, err := codec.MarshalJSONIndent(types.ModuleCdc, valReq)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+	return res, nil
 }
 
 //nolint: exhaustivestruct
@@ -466,6 +552,41 @@ func TestPendingValsetRequests(t *testing.T) {
 	}
 }
 
+// lastPendingValsetRequest gets a list of validator sets that this validator has not signed
+// limited by 100 sets per request.
+func lastPendingValsetRequest(ctx sdk.Context, operatorAddr string, keeper Keeper) ([]byte, error) {
+	addr, err := sdk.AccAddressFromBech32(operatorAddr)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "address invalid")
+	}
+
+	var pendingValsetReq []*types.Valset
+	keeper.IterateValsets(ctx, func(_ []byte, val *types.Valset) bool {
+		// foundConfirm is true if the operatorAddr has signed the valset we are currently looking at
+		foundConfirm := keeper.GetValsetConfirm(ctx, val.Nonce, addr) != nil
+		// if this valset has NOT been signed by operatorAddr, store it in pendingValsetReq
+		// and exit the loop
+		if !foundConfirm {
+			pendingValsetReq = append(pendingValsetReq, val)
+		}
+		// if we have more than 100 unconfirmed requests in
+		// our array we should exit, TODO pagination
+		if len(pendingValsetReq) > 100 {
+			return true
+		}
+		// return false to continue the loop
+		return false
+	})
+	if len(pendingValsetReq) == 0 {
+		return nil, nil
+	}
+	res, err := codec.MarshalJSONIndent(types.ModuleCdc, pendingValsetReq)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+	return res, nil
+}
+
 //nolint: exhaustivestruct
 // TODO: check that it actually returns a batch that has NOT been signed, not just any batch
 func TestLastPendingBatchRequest(t *testing.T) {
@@ -544,6 +665,32 @@ func TestLastPendingBatchRequest(t *testing.T) {
 	}
 }
 
+// lastPendingBatchRequest gets the latest batch that has NOT been signed by operatorAddr
+func lastPendingBatchRequest(ctx sdk.Context, operatorAddr string, keeper Keeper) ([]byte, error) {
+	addr, err := sdk.AccAddressFromBech32(operatorAddr)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "address invalid")
+	}
+
+	var pendingBatchReq *types.OutgoingTxBatch
+	keeper.IterateOutgoingTXBatches(ctx, func(_ []byte, batch *types.InternalOutgoingTxBatch) bool {
+		foundConfirm := keeper.GetBatchConfirm(ctx, batch.BatchNonce, batch.TokenContract, addr) != nil
+		if !foundConfirm {
+			pendingBatchReq = batch.ToExternal()
+			return true
+		}
+		return false
+	})
+	if pendingBatchReq == nil {
+		return nil, nil
+	}
+	res, err := codec.MarshalJSONIndent(types.ModuleCdc, pendingBatchReq)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+	return res, nil
+}
+
 //nolint: exhaustivestruct
 func createTestBatch(t *testing.T, input TestInput) {
 	var (
@@ -620,6 +767,31 @@ func TestQueryAllBatchConfirms(t *testing.T) {
 	assert.JSONEq(t, string(expectedJSON), string(batchConfirms), "json is equal")
 }
 
+// allBatchConfirms returns all the confirm messages for a given nonce
+// When nothing found an empty json array is returned. No pagination.
+func queryAllBatchConfirms(ctx sdk.Context, nonceStr string, tokenContract string, keeper Keeper) ([]byte, error) {
+	nonce, err := types.UInt64FromString(nonceStr)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, err.Error())
+	}
+	contract, err := types.NewEthAddress(tokenContract)
+
+	var confirms []types.MsgConfirmBatch
+	keeper.IterateBatchConfirmByNonceAndTokenContract(ctx, nonce, *contract, func(_ []byte, c types.MsgConfirmBatch) bool {
+		confirms = append(confirms, c)
+		return false
+	})
+	if len(confirms) == 0 {
+		return nil, nil
+	}
+	res, err := codec.MarshalJSONIndent(types.ModuleCdc, confirms)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+
+	return res, nil
+}
+
 //nolint: exhaustivestruct
 func TestQueryLogicCalls(t *testing.T) {
 	input := CreateTestEnv(t)
@@ -677,6 +849,49 @@ func TestQueryLogicCalls(t *testing.T) {
 	require.NoError(t, err)
 
 	require.NoError(t, err)
+}
+
+// lastPendingLogicCallRequest gets the latest call that has NOT been signed by operatorAddr
+func lastPendingLogicCallRequest(ctx sdk.Context, operatorAddr string, keeper Keeper) ([]byte, error) {
+	addr, err := sdk.AccAddressFromBech32(operatorAddr)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "address invalid")
+	}
+
+	var pendingLogicCalls *types.OutgoingLogicCall
+	keeper.IterateOutgoingLogicCalls(ctx, func(_ []byte, call *types.OutgoingLogicCall) bool {
+		foundConfirm := keeper.GetLogicCallConfirm(ctx, call.InvalidationId, call.InvalidationNonce, addr) != nil
+		if !foundConfirm {
+			pendingLogicCalls = call
+			return true
+		}
+		return false
+	})
+	if pendingLogicCalls == nil {
+		return nil, nil
+	}
+	res, err := codec.MarshalJSONIndent(types.ModuleCdc, pendingLogicCalls)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+	return res, nil
+}
+
+// Gets MaxResults logic calls from store.
+func lastLogicCallRequests(ctx sdk.Context, keeper Keeper) ([]byte, error) {
+	var calls []*types.OutgoingLogicCall
+	keeper.IterateOutgoingLogicCalls(ctx, func(_ []byte, call *types.OutgoingLogicCall) bool {
+		calls = append(calls, call)
+		return len(calls) == MaxResults
+	})
+	if len(calls) == 0 {
+		return nil, nil
+	}
+	res, err := codec.MarshalJSONIndent(types.ModuleCdc, calls)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+	return res, nil
 }
 
 //nolint: exhaustivestruct
@@ -796,6 +1011,27 @@ func TestQueryBatch(t *testing.T) {
 
 	// TODO: this test is failing on the empty representation of valset members
 	assert.JSONEq(t, string(expectedJSON), string(batch), string(batch))
+}
+
+// queryBatch gets a batch by tokenContract and nonce
+func queryBatch(ctx sdk.Context, nonce string, tokenContract string, keeper Keeper) ([]byte, error) {
+	parsedNonce, err := types.UInt64FromString(nonce)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, err.Error())
+	}
+	contract, err := types.NewEthAddress(tokenContract)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, err.Error())
+	}
+	foundBatch := keeper.GetOutgoingTXBatch(ctx, *contract, parsedNonce)
+	if foundBatch == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "Can not find tx batch")
+	}
+	res, err := codec.MarshalJSONIndent(types.ModuleCdc, foundBatch.ToExternal())
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, err.Error())
+	}
+	return res, nil
 }
 
 //nolint: exhaustivestruct
@@ -930,6 +1166,23 @@ func TestQueryERC20ToDenom(t *testing.T) {
 	assert.Equal(t, correctBytes, queriedDenom)
 }
 
+func queryERC20ToDenom(ctx sdk.Context, ERC20 string, keeper Keeper) ([]byte, error) {
+	ethAddr, err := types.NewEthAddress(ERC20)
+	if err != nil {
+		return nil, sdkerrors.Wrapf(err, "invalid ERC20 in query: %s", ERC20)
+	}
+	cosmos_originated, denom := keeper.ERC20ToDenomLookup(ctx, *ethAddr)
+	var response types.QueryERC20ToDenomResponse
+	response.CosmosOriginated = cosmos_originated
+	response.Denom = denom
+	bytes, err := codec.MarshalJSONIndent(types.ModuleCdc, response)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	} else {
+		return bytes, nil
+	}
+}
+
 //nolint: exhaustivestruct
 func TestQueryDenomToERC20(t *testing.T) {
 	var (
@@ -952,6 +1205,22 @@ func TestQueryDenomToERC20(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Equal(t, correctBytes, queriedERC20)
+}
+
+func queryDenomToERC20(ctx sdk.Context, denom string, keeper Keeper) ([]byte, error) {
+	cosmos_originated, erc20, err := keeper.DenomToERC20Lookup(ctx, denom)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+	var response types.QueryDenomToERC20Response
+	response.CosmosOriginated = cosmos_originated
+	response.Erc20 = erc20.GetAddress()
+	bytes, err := codec.MarshalJSONIndent(types.ModuleCdc, response)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	} else {
+		return bytes, nil
+	}
 }
 
 //nolint: exhaustivestruct
@@ -1069,4 +1338,31 @@ func TestQueryPendingSendToEth(t *testing.T) {
 	  `)
 
 	assert.JSONEq(t, string(expectedJSON), string(response), "json is equal")
+}
+
+func queryPendingSendToEth(ctx sdk.Context, senderAddr string, k Keeper) ([]byte, error) {
+	batches := k.GetOutgoingTxBatches(ctx)
+	unbatched_tx := k.GetUnbatchedTransactions(ctx)
+	res := types.QueryPendingSendToEthResponse{
+		TransfersInBatches: []*types.OutgoingTransferTx{},
+		UnbatchedTransfers: []*types.OutgoingTransferTx{},
+	}
+	for _, batch := range batches {
+		for _, tx := range batch.Transactions {
+			if tx.Sender.String() == senderAddr {
+				res.TransfersInBatches = append(res.TransfersInBatches, tx.ToExternal())
+			}
+		}
+	}
+	for _, tx := range unbatched_tx {
+		if tx.Sender.String() == senderAddr {
+			res.UnbatchedTransfers = append(res.UnbatchedTransfers, tx.ToExternal())
+		}
+	}
+	bytes, err := codec.MarshalJSONIndent(types.ModuleCdc, res)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	} else {
+		return bytes, nil
+	}
 }
